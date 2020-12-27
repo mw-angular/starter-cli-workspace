7b4f7385121971dcae589667aef7a144b1863b0b
Andrey Korovin <misticwonder@gmail.com>
2020-12-27 13:25:14 +0200

    #0 design system button component

---
+++ libs/design-system/src/lib/modules/ds-button/components/ds-button/ds-button.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-button',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './ds-button.component.html',
+})
+export class DsButtonComponent {
+}
--- libs/design-system/src/public-api.ts
+++ libs/design-system/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of design-system
- */
-
-export * from './lib/design-system.service';
-export * from './lib/design-system.component';
-export * from './lib/design-system.module';
+export * from './lib/modules/ds-button/ds-button.module';
