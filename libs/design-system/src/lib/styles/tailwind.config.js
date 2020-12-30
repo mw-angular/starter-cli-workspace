f64f2e4f010945ebb7456944fd5e72c1e9b7ec3a
Andrey Korovin <misticwonder@gmail.com>
2020-12-30 14:40:16 +0200

    #0 styles with tailwind

--- libs/design-system/src/lib/styles/tailwind.config.js
+++ libs/design-system/src/lib/styles/tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  purge: [
+    `libs/design-system/src/**/*.html`,
+    `libs/design-system/src/**/*.component.ts`
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
