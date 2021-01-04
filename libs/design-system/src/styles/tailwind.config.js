f78308895f98f00eebd3bfba7992e317e73a57f0
Andrey Korovin <misticwonder@gmail.com>
2021-01-04 21:27:19 +0200

    #0 font for tailwind

--- libs/design-system/src/styles/tailwind.config.js
+++ libs/design-system/src/styles/tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  prefix: 'tw-',
   purge: [
     `libs/design-system/src/**/*.html`,
     `libs/design-system/src/**/*.component.ts`
